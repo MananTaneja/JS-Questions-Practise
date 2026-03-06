3716f58f88253879ee3f269444e3de6643abed83
MananTaneja <manantaneja007@gmail.com>
2026-03-07 00:27:08 +0530

    feat: solution

--- LC/encodeDecodeStrings.go
+++ LC/encodeDecodeStrings.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	input := []string{"first", "second", "third"}
@@ -14,16 +17,54 @@ func main() {
 	fmt.Printf("decoded string")
 
 	for _, output := range decodedString {
-		fmt.Printf("%s", output)
+		fmt.Printf(" %s", output)
 	}
 }
 
 func encode(incoming []string) string {
-	return ""
+	encoded := ""
+
+	for _, str := range incoming {
+		prefix := paddInt(len(str))
+
+		encoded += prefix + str
+	}
+
+	return encoded
 }
 
 func decode(incoming string) []string {
 	decoded := []string{}
 
+	i, n := 0, len(incoming)
+
+	for i < n {
+		prefix := incoming[i : i+4]
+		i += 4
+
+		length, _ := strconv.Atoi(prefix)
+
+		word := incoming[i : i+length]
+
+		decoded = append(decoded, word)
+
+		i += length
+	}
+
 	return decoded
+}
+
+func paddInt(number int) string {
+	st := strconv.Itoa(number)
+
+	switch len(st) {
+	case 0:
+		return "0000"
+	case 1:
+		return "000" + st
+	case 2:
+		return "00" + st
+	default:
+		return st
+	}
 }
